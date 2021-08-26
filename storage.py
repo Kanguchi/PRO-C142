816f74f198df085614766eb399606546e9f54cfa
Kanguchi <70295418+Kanguchi@users.noreply.github.com>
2021-08-26 11:42:56 -0400

    Add files via upload

--- storage.py
+++ storage.py
@@ -7,5 +7,9 @@ with open("articles.csv",encoding="UTF8") as f:
     data = list(reader)
     all_articles = data[1:]
 
+for articles in all_articles:
+    if articles[14] == 'pt':
+        all_articles.remove(articles)
+
 liked_articles = []
 not_liked_articles = []
